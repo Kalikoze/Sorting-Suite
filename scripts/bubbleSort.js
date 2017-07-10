761cfb457b2bbc290a72c526d77a34f90cb78e28
Travis Rollins <TROLL59910@aol.com>
2017-07-10 16:18:02 -0600

    Finish bubblesort function and tests

---
+++ scripts/bubbleSort.js
@@ -0,0 +1,16 @@
+const letters = ["d", "b", "a", "c"]
+
+const bubbleSort = array => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - i; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j+1]] = [array[j+1], array[j]]
+      }
+    }
+  }
+  return array;
+}
+
+bubbleSort(letters);
+
+module.exports = bubbleSort;
