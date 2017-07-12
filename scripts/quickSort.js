d4640bc97671abe881463e4edb56beedb6ee5b87
Travis Rollins <TROLL59910@aol.com>
2017-07-12 11:37:32 -0600

    Add quickSort functionality.  Write tests for quickSort

--- scripts/quickSort.js
+++ scripts/quickSort.js
@@ -1,1 +1,25 @@
+const letters = ["d", "b", "a", "c"]
 
+const quickSort = (array) => {
+  var pivot = array[0];
+  var leftSide = [];
+  var rightSide = [];
+
+  if(array.length < 2) {
+    return array;
+  }
+
+  for(var i = 1; i < array.length; i++) {
+    if(array[i] < pivot) {
+      leftSide.push(array[i]);
+    } else {
+      rightSide.push(array[i]);
+    }
+  }
+
+  return quickSort(leftSide).concat(pivot, quickSort(rightSide));
+}
+
+quickSort(letters);
+
+module.exports = quickSort
